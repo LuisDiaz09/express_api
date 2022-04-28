c04685d6bee05e3874d3dfeb4aab98b2b6f149ee
Luis <launchx04206@innovaccion.mx>
2022-04-27 19:35:23 -0500

    5. Creando un endpoint que regresa una lista, a partir del método GET

--- app.js
+++ app.js
@@ -9,13 +9,15 @@ const port = 3000
 //HTTP METHODS
 app.get('/v1/explorers', (req, res) => {
     console.log(`Api Explorers GET ALL requests ${new Date()}`)
-    const explorer1 = {id: 1, name: "Carlo"}
-    const explorer2 = {id: 1, name: "Carlo"}
-    const explorer3 = {id: 1, name: "Carlo"}
-    const explorer4 = {id: 1, name: "Carlo"}
+    const explorer1 = {id: 1, name: "Luis"}
+    const explorer2 = {id: 2, name: "Angel"}
+    const explorer3 = {id: 3, name: "Juan"}
+    const explorer4 = {id: 4, name: "Miguel"}
     const explorers = [explorer1, explorer2, explorer3, explorer4]
     res.status(200).json(explorers)
   })
+
+  
 
 // Con esto inicializamos esta app
 app.listen(port, () => {
